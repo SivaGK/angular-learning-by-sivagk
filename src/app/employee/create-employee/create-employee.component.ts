8f8aa95c9c9444bd33f931c2a914d5fe55e75757
SivaGK <rits_514@yahoo.co.in>
2019-06-21 17:09:29 +0530

    move validation messages to the component class

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -13,14 +13,14 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email: [''],
+      email: ['', [Validators.required]],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['beginner']
+        skillName: ['', [Validators.required]],
+        experienceInYears: ['', [Validators.required]],
+        proficiency: ['beginner', [Validators.required]]
       })
     });
-    
+
     // Subscribe to valueChanges observable for one controle.
     //this.employeeForm.get('fullName').valueChanges.subscribe(v => { console.log(v); })
 
@@ -40,7 +40,8 @@ export class CreateEmployeeComponent implements OnInit {
   }
   onSubmit(): void {
     console.log(this.employeeForm.value);
-    this.logKeyValuePairs(this.employeeForm);
+    //Loop through all form controls in formgroup in reactive form
+    //this.logValidationErrors(this.employeeForm);
   }
   onLoadDataClick(): void {
     //use setValue() to update all form controls and patchValue() to update a sub-set of form controls 
@@ -54,21 +55,69 @@ export class CreateEmployeeComponent implements OnInit {
       //   proficiency: 'beginner'
       // }
     });
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
   }
-  logKeyValuePairs(group: FormGroup): void {
-  // loop through each key in the FormGroup
-  Object.keys(group.controls).forEach((key: string) => {
-    // Get a reference to the control using the FormGroup.get() method
-    const abstractControl = group.get(key);
-    // If the control is an instance of FormGroup i.e a nested FormGroup
-    // then recursively call this same method (logKeyValuePairs) passing it
-    // the FormGroup so we can get to the form controls in it
-    if (abstractControl instanceof FormGroup) {
-      this.logKeyValuePairs(abstractControl);
-      // If the control is not a FormGroup then we know it's a FormControl
-    } else {
-      console.log('Key = ' + key + ' && Value = ' + abstractControl.value);
-    }
-  });
-}
+  logValidationErrors(group: FormGroup): void {
+    // loop through each key in the FormGroup
+    Object.keys(group.controls).forEach((key: string) => {
+      // Get a reference to the control using the FormGroup.get() method
+      const abstractControl = group.get(key);
+      // If the control is an instance of FormGroup i.e a nested FormGroup
+      // then recursively call this same method (logKeyValuePairs) passing it
+      // the FormGroup so we can get to the form controls in it
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+        // If the control is not a FormGroup then we know it's a FormControl
+      } else {
+        // Clear the existing validation errors
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid) {
+          // Get all the validation messages of the form control
+          // that has failed the validation
+          const messages = this.validationMessages[key];
+          // Find which validation has failed. For example required,
+          // minlength or maxlength. Store that error message in the
+          // formErrors object. The UI will bind to this object to
+          // display the validation errors
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    });
+  }
+  //move validation messages to the component class
+  // This object will hold the messages to be displayed to the user
+  // Notice, each key in this object has the same name as the
+  // corresponding form control
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'skillName': '',
+    'experienceInYears': '',
+    'proficiency': ''
+  };
+  // This object contains all the validation messages for this form
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
 }
